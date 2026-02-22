b7f1c2245f020395fb8844ab04caff2cb64190a5
matina <matinamhzn@gmail.com>
2026-02-22 16:28:18 +0545

    Frontend

---
+++ vite/src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './App.css'
+
+
+const App = () => {
+
+
+
+
+  return (
+    <div>
+      <div className="box">
+        <div className="container">
+          <h1>To-Do List</h1>
+          <div className="input-box">
+            <input type="text" placeholder='Add a to-do' name="" id="" />
+            <button>Add</button>
+          </div>
+        </div>
+      </div>
+
+
+    <ul className='lists'>
+
+    </ul>
+    </div>
+  )
+}
+
+export default App
