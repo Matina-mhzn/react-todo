03368351a21698152d895fc99d3b62ace636d892
matina <matinamhzn@gmail.com>
2026-03-09 22:43:27 +0545

    done

--- vite/src/App.jsx
+++ vite/src/App.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
 import './App.css'
+import { useState } from 'react';
+
 
 
 const App = () => {
+
+  const [tasks, setTasks] = useState([]);
+  const [newtask, setNewtask] = useState("");
+
+  function handleInputChange(event) {
+    setNewtask(event.target.value);
+  }
+  function addtask() {
+    if (newtask.trim() !== "") {
+      setTasks(t => [...t, newtask]);
+      setNewtask("");
+    }
+  }
+  function dltask(index) {
+    const updatedtask = tasks.filter((_, i) => i !== index);
+    setTasks(updatedtask);
+  }
+
+
 
 
 
@@ -13,16 +34,27 @@ const App = () => {
         <div className="container">
           <h1>To-Do List</h1>
           <div className="input-box">
-            <input type="text" placeholder='Add a to-do' name="" id="" />
-            <button>Add</button>
+            <input type="text" placeholder='Add a to-do' name="" id="" value={newtask} onChange={handleInputChange} />
+            <button onClick={addtask}>Add</button>
+          </div>
+      <br/>
+          <div className="tasks">
+            <ol>
+              {tasks.map((task, index) =>
+                <li key={index}>
+                  <div className="task">
+                  <span className='text'>{task}</span>
+                  <button className='dltbtn' onClick={() => dltask(index)}>Delete</button>
+                  </div>
+                </li>
+              )}
+            </ol>
           </div>
         </div>
       </div>
 
 
-    <ul className='lists'>
 
-    </ul>
     </div>
   )
 }
